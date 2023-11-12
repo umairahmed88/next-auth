66b469c6b22cfc5b947483e28906393e39f2b875
umairahmed88 <umairahmed_88@ymail.com>
2023-11-12 14:38:50 +0500

    Added navbar to the application

--- src/app/components/Nav.tsx
+++ src/app/components/Nav.tsx
@@ -1,12 +1,20 @@
-// import React from "react";
+import Link from "next/link";
+import React from "react";
 
-// export default function Nav() {
-// 	return (
-// 		<header>
-// 			<nav
-// 				className='flex items-center justify-between p-6 lg:p-8 h-20 border border-b-gray-600'
-// 				aria-label='Global'
-// 			></nav>
-// 		</header>
-// 	);
-// }
+export default function Nav() {
+	return (
+		<header>
+			<nav
+				className='flex items-center justify-between p-6 lg:p-8 h-20 border border-b-gray-600'
+				aria-label='Global'
+			>
+				<div className=' flex lg:flex-1'>
+					<a href='/' className='-m-1.5 p-1.5'>
+						Next Js Authentication
+					</a>
+				</div>
+				<Link href='/dashboard'>Dashboard</Link>
+			</nav>
+		</header>
+	);
+}
