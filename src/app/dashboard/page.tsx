88f927591640f63f5e9eeb57e6d3be05cc64d8db
umairahmed88 <umairahmed_88@ymail.com>
2023-11-12 16:34:50 +0500

    Added db with xata

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,15 @@
+import { getXataClient } from "@/xata";
 import React from "react";
 
-export default function DashboardPage() {
-	return <div>Dashboard Page</div>;
+export default async function DashboardPage() {
+	const xataClient = getXataClient();
+	const folders = await xataClient.db.folders.getMany();
+	return (
+		<div>
+			<h1>Dashboard Page</h1>
+			{folders.map((folder) => (
+				<p key={folder.id}>{folder.name}</p>
+			))}
+		</div>
+	);
 }
