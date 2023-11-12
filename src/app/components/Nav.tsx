27433da9571f6c66b06b072569e15b1a197928f9
umairahmed88 <umairahmed_88@ymail.com>
2023-11-12 14:32:13 +0500

    insatlled next js and added dashboard page

---
+++ src/app/components/Nav.tsx
@@ -0,0 +1,12 @@
+// import React from "react";
+
+// export default function Nav() {
+// 	return (
+// 		<header>
+// 			<nav
+// 				className='flex items-center justify-between p-6 lg:p-8 h-20 border border-b-gray-600'
+// 				aria-label='Global'
+// 			></nav>
+// 		</header>
+// 	);
+// }
